4da0edbcee1e8d592dc88cab07fb8c990e1489de
HarkunVladislav <63816951+HarkunVladislav@users.noreply.github.com>
2020-05-20 12:20:15 +0300

    Delete ThreadOutput.java

--- 4sem /КПП/laba4/Thread/ThreadOutput.java
+++
@@ -1,9 +0,0 @@
-package Thread;
-
-public class ThreadOutput {
-	public static synchronized void StringOutput(String str) {
-		for(int j = 0; j < str.length(); ++j) {
-			System.out.printf("%c", str.charAt(j));
-		}
-	}
-}
