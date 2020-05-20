328bf1c6d4e3f4ba9c8f911fd9b588915dc8d8d6
HarkunVladislav <63816951+HarkunVladislav@users.noreply.github.com>
2020-05-20 12:14:03 +0300

    Add files via upload

---
+++ 4sem /КПП/laba4/Резьба/ThreadOutput.java
@@ -0,0 +1,9 @@
+package Thread;
+
+public class ThreadOutput {
+	public static synchronized void StringOutput(String str) {
+		for(int j = 0; j < str.length(); ++j) {
+			System.out.printf("%c", str.charAt(j));
+		}
+	}
+}
