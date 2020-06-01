20c161d00fd856239b06d50bbd0df1209f50af90
HarkunVladislav <63816951+HarkunVladislav@users.noreply.github.com>
2020-06-01 19:57:03 +0300

    Add files via upload

---
+++ 4sem /КПП/laba6/Part1/laba6_1.scala
@@ -0,0 +1,7 @@
+
+
+object laba6_1 extends App{
+  val string: String = "*$(#*$(#$3 43jkklfnd fjdsfjdkfjdk Jk jkfj kfj".split(' ').map(_.capitalize).mkString(" ")
+  
+  printf("%s", string:String)
+}
